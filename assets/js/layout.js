271db7b3c0a1f442ea00e33a837309e7a72d30aa
Nastinka <Anastasiagumenuk96@gmail.com>
2020-07-30 18:20:34 +0300

    .

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -6,10 +6,10 @@ window.onload = function() {
 
         if(div.style.background != "black") {
             document.getElementById("inner").hidden = true;
-            document.getElementById("login-form").hidden = true;
             div.style.background = "black";
             div.style.height = "100vh";
             div.style.width = "100%";
+            document.getElementById("login-form").hidden = true;
         }
         else {
             div.style.background = "transparent";
